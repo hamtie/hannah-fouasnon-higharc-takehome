295861548d4aa6e054eeacf221ecc46e1212b70a
Hannah Fouasnon <hamtie@fermat.attlocal.net>
2024-10-29 16:05:16 -0500

    Setup tailwind and add header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+'use client'
+import smoothieLogo from "./assets/icons8-smoothie.svg";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    return (
+        <header className="bg-white">
+            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+                <div className="flex lg:flex-1">
+                    <a href="#" className="m-1 p-1">
+                        <span className="sr-only">Smoothie Recipes</span>
+                        <img
+                            alt="Smoothie Recipes"
+                            src={smoothieLogo}
+                            className="h-8 w-auto"
+                        />
+                    </a>
+                </div>
+                <h2 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                    Smoothie Recipes
+                </h2>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            </nav>
+        </header>
+    )
 }
-
-export default App
